8846587adaeb206b33f01231ffc359c3e69e43f3
Alex Bannon <alexbannon@gmail.com>
2021-02-22 21:29:10 -0500

    Create navigateToAllocation.js

---
+++ navigateToAllocation.js
@@ -0,0 +1,19 @@
+function navigate() {
+  var paths = window.location.pathname.split('/');
+  var order = paths[paths.length - 1];
+
+  $('.sales-order-tab-link')[3].click()
+  window.setTimeout(() => {
+    var numChildren = $('#sales-order-invoices').children('table').children('tbody').children().length;
+    if (numChildren > 1) {
+      console.log('error too many invoices for order: ' + order);
+    } else {
+      var invoiceNum = $('#sales-order-invoices').children('table').children('tbody').children().children()[0].innerHTML;
+      var url = "/shop/invoices/" + invoiceNum + "/allocation";
+      window.open(url);
+    }
+  }, 500)
+}
+
+
+window.onload=navigate;
